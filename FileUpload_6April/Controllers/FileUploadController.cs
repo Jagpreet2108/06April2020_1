e2aece0de47ff04e6a7477357ddacbf6578471e8
Jagpreet2108 <jagpreetkaur2108@gmail.com>
2020-04-06 21:50:36 +0530

    FinalCommit

---
+++ FileUpload_6April/Controllers/FileUploadController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using FileUpload_6April.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FileUpload_6April.Controllers
+{
+    public class FileUploadController : Controller
+    {
+
+        private readonly IWebHostEnvironment _env;
+        private readonly ILogger<FileUploadController> _logger;
+
+        public FileUploadController(ILogger<FileUploadController> logger, IWebHostEnvironment env)
+        {
+            _logger = logger;
+            _env = env;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UploadFile(FileUpload fileUpload)
+        {
+            string strpath = System.IO.Path.GetExtension(fileUpload.FormFile.FileName);
+
+            if (fileUpload.FormFile != null)
+            {
+
+                string FilePath = $"{_env.WebRootPath}/images/{fileUpload.FormFile.FileName}";
+                var stream = System.IO.File.Create(FilePath);
+                fileUpload.FormFile.CopyTo(stream);
+                return Redirect("/FileUpload/Success");
+
+            }
+            return Redirect("/");
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ FileUpload_6April/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FileUpload_6April.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
